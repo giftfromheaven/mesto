a9b26c78c7f7962c2409c2f48083e08d26f36fca
Ilya <72022227+giftfromheaven@users.noreply.github.com>
2021-08-01 18:56:06 +0300

    Add files via upload

---
+++ src/components/UserInfo.js
@@ -0,0 +1,22 @@
+class UserInfo {
+  constructor(name, work) {
+    this._name = name;
+    this._work = work;
+  }
+
+  getUserInfo() { 
+    const userInfo = {
+      name: this._name.textContent,
+      work: this._work.textContent,
+    };
+
+    return userInfo;
+  }
+
+  setUserInfo(name, work) {
+    this._name.textContent = name;
+    this._work.textContent = work;
+  }
+}
+
+export default UserInfo;
---
+++ src/pages/index.js
@@ -0,0 +1,90 @@
+import { initialCards } from '../components/initial-cards.js';
+import FormValidator from '../components/FormValidator.js';
+import Card from '../components/Card.js';
+import Section from '../components/Section.js';
+import PopupWithImage from '../components/PopupWithImage.js';
+import PopupWithForm from '../components/PopupWithForm.js';
+import UserInfo from '../components/UserInfo.js';
+import '../pages/index.css';
+
+const config = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error',
+};
+
+// Popup profile element
+const popupProfile = document.querySelector('#popup-profile-data');
+const openProfilePopupButton = document.querySelector('.profile__edit');
+
+// Popup add card
+const popupAddCard = document.querySelector('#element');
+const openCardPopupButton = document.querySelector('.profile__add-button');
+
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__job');
+const inputName = document.querySelector('#name');
+const inputJob = document.querySelector('#job');
+const formProfile = document.querySelector("form[name='info-form']");
+const formPlace = document.querySelector("form[name='new-card']");
+
+const cardsContainer = document.querySelector('.elements');
+
+const validatorProfile = new FormValidator(config, formProfile);
+validatorProfile.enableValidation();
+const validatorPlace = new FormValidator(config, formPlace);
+validatorPlace.enableValidation();
+
+const popupIncludesImage = document.querySelector('#popup-image');
+const popupImage = new PopupWithImage(popupIncludesImage);
+popupImage.setEventListeners();
+
+function open(name, link) {
+  popupImage.open(name, link);
+}
+
+function addNewCard({ place, link }) {
+  const card = new Card(place, link, '#element-template', open).createCard();
+  cardsContainer.prepend(card);
+}
+
+function saveProfileData({ name, work }) {
+  user.setUserInfo(name, work);
+}
+
+const popupFormProfile = new PopupWithForm(popupProfile, saveProfileData);
+const popupAddCardObj = new PopupWithForm(popupAddCard, addNewCard);
+popupFormProfile.setEventListeners();
+popupAddCardObj.setEventListeners();
+
+const user = new UserInfo(profileName, profileJob);
+
+const cardList = new Section(
+  {
+    items: initialCards,
+    renderer: (item) => {
+      const card = new Card(item.name, item.link, '#element-template', open).createCard();
+      cardList.addItem(card);
+    },
+  },
+  cardsContainer,
+);
+
+cardList.renderItems();
+
+// Открытие попапа профиля
+openProfilePopupButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  popupFormProfile.open();
+  inputName.value = profileName.textContent;
+  inputJob.value = profileJob.textContent;
+});
+
+// Открытие попапа для добавления карточки
+openCardPopupButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  popupAddCardObj.open();
+});
