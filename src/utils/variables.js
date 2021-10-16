d6297b420444dad0b2edad43d9cba2e1e8e41dc0
Ilya <72022227+giftfromheaven@users.noreply.github.com>
2021-10-16 22:25:45 +0300

    Add files via upload

---
+++ src/utils/variables.js
@@ -0,0 +1,32 @@
+export const userIformation = {
+  userName: '.profile__name',
+  userAbout: '.profile__occupation',
+  userAvatar: '.profile__avatar',
+  userId: null,
+};
+
+export const popups = {
+  avatar: '#edit-avatar',
+  profile: '#edit-profile',
+  addPlace: '#add-place',
+  image: '#image-popup',
+  delete: '#delete-conformation',
+};
+
+export const forms = {
+  avatar: '#avatar-edit-form',
+  plofile: '#plofile-edit-form',
+  place: '#add-element-form',
+  delete: '#delete-conformation-form',
+};
+
+export const buttons = {
+  avatar: document.querySelector('.profile__avatar'),
+  editProfile: document.querySelector('.profile__edit-button'),
+  addPlace: document.querySelector('.profile__add-button'),
+};
+
+export const template = {
+  listElements: '.elements__list',
+  element: '.element-template',
+};
